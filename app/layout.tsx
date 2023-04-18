0e11ef3e0160b773a22375eda17d27a08d13b6f4
Surjith S M <surjithctly@gmail.com>
2023-04-18 21:55:38 +0530

    setting up fonts, colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,35 @@
-import './globals.css'
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
+import { cx } from "@/lib/utils";
 
 export const metadata = {
-  title: 'HR Web App',
-  description: 'The new home of Hey Rebekah powered by Sanity.io, Next.js, and Tailwind CSS. Made with ❤️ in partnership with W3CTemplates.',
-}
+  title: "HR Web App",
+  description:
+    "The new home of Hey Rebekah powered by Sanity.io, Next.js, and Tailwind CSS. Made with ❤️ in partnership with W3CTemplates.",
+};
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "700"],
+  display: "swap",
+});
+
+const apfelGrotezk = localFont({
+  src: "./assets/fonts/apfel-grotezk-fett.woff2",
+  variable: "--font-apfel-grotezk",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cx(poppins.variable, apfelGrotezk.variable)}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-poppins)", ...fontFamily.sans],
+        display: [
+          "var(--font-apfel-grotezk)",
+          "var(--font-poppins)",
+          ...fontFamily.sans,
+        ],
+      },
+      colors: {
+        black: "#020512",
+        "dark-blue": "#040b29",
+        blue: "#041746",
+        pink: "#f34dc3",
+        yellow: "#ffcf33",
+        green: "#1aefc4",
+        aqua: "#08b2e3",
+        "light-grey": "#e0e0e0",
       },
     },
   },
   plugins: [],
-}
+};
