7717dde0f215b06f0f474d1cde81eab0c267aecc
Sam Rizvi <srizvi@users.noreply.github.com>
2023-04-18 03:33:57 -0700

    T-9201, T9202, T9206, T-9355

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "sanity";
 import { deskTool} from "sanity/desk";
 import schemas from "./sanity/schemas";
+import { scheduledPublishing } from "@sanity/scheduled-publishing";
+import { media } from "sanity-plugin-media";
+import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
+import { codeInput } from "@sanity/code-input";
+import { giphyAssetSourcePlugin } from "sanity-plugin-asset-source-giphy";
+import { openaiImageAsset } from "sanity-plugin-asset-source-openai";
 
 const config = defineConfig({
   projectId: 'smx99abf',
@@ -8,7 +14,19 @@ const config = defineConfig({
   title: 'Hey Rebekah',
   apiVersion: '2023-04-17',
   basePath: '/studio',
-  plugins: [deskTool()],
+  plugins: [
+    deskTool(),
+    scheduledPublishing(),
+    media(),
+    unsplashImageAsset(),
+    codeInput(),
+    giphyAssetSourcePlugin({
+      apiKey: "4Gi7oe0bE9zeStPSMpaqb98SJybKbKTz"
+    }),
+    openaiImageAsset({
+      API_KEY:"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"
+    }),
+  ],
   schema: { types: schemas }
 })
 
