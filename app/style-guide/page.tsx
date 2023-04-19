4e590e39c780b6247e0a149633f2b61936adbfca
Surjith S M <surjithctly@gmail.com>
2023-04-19 22:28:21 +0530

    setting up

--- app/style-guide/page.tsx
+++ app/style-guide/page.tsx
@@ -1,4 +1,5 @@
-import { H1, H4 } from '@/components/ui/typography/heading';
+import { H1, H2, H3, H4, H5, H6 } from "@/components/ui";
+import { Container, Badge, Spacer } from "@/components/ui";
 
 export default function StyleGuide() {
   //Placeholder to create route
@@ -14,10 +15,82 @@ export default function StyleGuide() {
           aliqua.
         </p>
       </div>
-      <div className="mt-10">
-        <H1>Title here</H1>
-        <H4>Title here</H4>
-      </div>
+      <Container>
+        <div className="mt-10">
+          <H4>Colors</H4>
+          <div className="flex flex-wrap gap-10 mt-6 border-t pt-6">
+            <div className="w-16 h-16 bg-black rounded"></div>
+            <div className="w-16 h-16 bg-white shadow rounded"></div>
+            <div className="w-16 h-16 bg-light-grey rounded"></div>
+            <div className="w-16 h-16 bg-blue rounded"></div>
+            <div className="w-16 h-16 bg-dark-blue rounded"></div>
+            <div className="w-16 h-16 bg-pink rounded"></div>
+            <div className="w-16 h-16 bg-yellow rounded"></div>
+            <div className="w-16 h-16 bg-green rounded"></div>
+            <div className="w-16 h-16 bg-aqua rounded"></div>
+          </div>
+        </div>
+
+        <div className="mt-16">
+          <H4>Typography</H4>
+          <div className="mt-6 border-t pt-6">
+            <Badge>h1</Badge>
+            <H1>This is a Heading</H1>
+            <Spacer />
+            <Badge>h2</Badge>
+            <H2>This is a Heading</H2>
+            <Spacer />
+            <Badge>h3</Badge>
+            <H3>This is a Heading</H3>
+            <Spacer />
+            <Badge>h4</Badge>
+            <H4>This is a Heading</H4>
+            <Spacer />
+            <Badge>h5</Badge>
+            <H5>This is a Heading</H5>
+            <Spacer />
+            <Badge>h6</Badge>
+            <H6>This is a Heading</H6>
+            <Spacer />
+            <p className="text-xl">
+              This is a lead paragraph. It stands out from regular paragraphs.
+            </p>
+            <Spacer />
+            <p>
+              Sample text is being used as a placeholder for real text that is
+              normally present. Sample text helps you understand how real text
+              may look on your website. Sample text is being used as a
+              placeholder for real text.
+            </p>
+          </div>
+        </div>
+        <div className="mt-10">
+          <H4>Badges</H4>
+          <div className="border-t pt-6 mt-4 flex flex-col gap-5 flex-wrap items-start">
+            <Badge>Default Badge</Badge>
+            <Badge variant="primary">Primary</Badge>
+            <Badge variant="secondary">Secondary</Badge>
+            <Badge variant="primary" size="lg">
+              Primary large
+            </Badge>
+            <Badge variant="primary" size="md">
+              Primary Medium
+            </Badge>
+            <Badge variant="primary" size="sm">
+              Primary Small
+            </Badge>
+            <Badge variant="secondary" size="lg">
+              Secondary Large
+            </Badge>
+            <Badge variant="secondary" size="md">
+              Secondary medium
+            </Badge>
+            <Badge variant="secondary" size="sm">
+              Secondary small
+            </Badge>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
--- components/ui/typography/heading.js
+++ components/ui/typography/heading.js
@@ -1,23 +1,27 @@
+import { cx } from "@/lib/utils";
+
+const commonClasses = "font-bold font-display tracking-tight";
+
 export function H1({ children }) {
-  return <h1>{children}</h1>;
+  return <h1 className={cx(commonClasses, "text-7xl")}>{children}</h1>;
 }
 
 export function H2({ children }) {
-  return <h1>{children}</h1>;
+  return <h2 className={cx(commonClasses, "text-6xl")}>{children}</h2>;
 }
 
 export function H3({ children }) {
-  return <h1>{children}</h1>;
+  return <h3 className={cx(commonClasses, "text-5xl")}>{children}</h3>;
 }
 
 export function H4({ children }) {
-  return <h1>{children}</h1>;
+  return <h4 className={cx(commonClasses, "text-4xl")}>{children}</h4>;
 }
 
 export function H5({ children }) {
-  return <h1>{children}</h1>;
+  return <h5 className={cx(commonClasses, "text-3xl")}>{children}</h5>;
 }
 
 export function H6({ children }) {
-  return <h1>{children}</h1>;
+  return <h6 className={cx(commonClasses, "text-2xl")}>{children}</h6>;
 }
