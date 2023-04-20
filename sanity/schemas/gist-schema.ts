13a30eb500a42aef4c52e98a3fe102d494d2d413
Sam Rizvi <srizvi@users.noreply.github.com>
2023-04-20 02:55:14 -0700

    Schema sam (#5)
    * Schema prototype
    
    * Misc. linting
    
    * Install desk tool. misc. bug fixes, package update
    
    * Misc. schema updates for group(s). Link T-9289

--- sanity/schemas/gist-schema.ts
+++ sanity/schemas/gist-schema.ts
@@ -7,148 +7,196 @@ const gists = {
   title: 'Gists',
   icon: FaFeatherAlt,
   type: 'document',
-  fields: [{
-    name: 'name',
-    title: 'Name',
-    description: 'The title of the post.',
-    type: 'string',
-  },
-  {
-    name: 'slug',
-    title: 'Slug',
-    description: 'The slug of the post.',
-    type: 'slug',
-    options: { source: 'name', maxLength: 96 },
-  },
-  {
-    name: 'approved',
-    title: 'Approved',
-    description: 'Is this post approved?',
-    type: 'boolean',
-  },
-  {
-    name: "featured",
-    title: "Mark as featured",
-    description: 'Is this a featured post?.',
-    type: "boolean"
-  },
-  {
-    name: "category",
-    title: "Category",
-    description: 'Select the most relevant category for this post.',
-    type: "array",
-    of: [{ type: "reference", to: { type: "category" } }]
-  },
-  {
-    name: "author",
-    title: "Author",
-    description: "The author of the post.",
-    type: "reference",
-    to: { type: "author" },
-  },
-  {
-    name: "featuredImage",
-    title: "Featured image",
-    description: "The really big image at the top of every post. You can add your own from Stable Diffusion or use the built-in Unsplash integration.",
-    type: "image",
-    fields: [
-      {
-        name: "caption",
-        type: "string",
-        title: "Image caption",
-        description: "Use this to add your Stable Diffusion prompt or attribute a source.",
+  groups: [
+    {
+      name: 'editorialWorkflow',
+      title: 'Edtorial Workflow',
+    },
+    {
+      name: 'writing',
+      title: 'Writing',
+      default: true,
+    },
+    {
+      name: 'meta',
+      title: 'Meta',
+    },
+    {
+      name: 'seo',
+      title: 'SEO',
+    },
+  ],
+  fields: [
+    {
+      name: "status",
+      title: "Status",
+      type: "string",
+      description: "Select the status of the document.",
+      group: 'editorialWorkflow',
+      options: {
+        list: [
+          { title: "Ideation", value: "ideation" },
+          { title: "Rough draft", value: "rough_draft" },
+          { title: "First draft", value: "first_draft" },
+          { title: "Final draft", value: "final_draft" },
+          { title: "Editing", value: "editing" },
+          { title: "Approved", value: "approved" }
+        ],
+        layout: "dropdown",
       },
-      {
-        name: "alt",
-        type: "string",
-        title: "Alternative text",
-        description: "E.g. A grumpy looking bald man on a chair."
+      validation: (Rule: any) => Rule.required()
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      description: 'The title of the post.',
+      type: 'string',
+      group: 'writing',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      description: 'The slug of the post.',
+      type: 'slug',
+      group: 'seo',
+      options: { source: 'name', maxLength: 96 },
+    },
+    {
+      name: 'approved',
+      title: 'Approved',
+      description: 'Is this post approved?',
+      type: 'boolean',
+      group: 'editorialWorkflow',
+    },
+    {
+      name: "featured",
+      title: "Mark as featured",
+      description: 'Is this a featured post?.',
+      type: "boolean",
+      group: 'editorialWorkflow',
+    },
+    {
+      name: "category",
+      title: "Category",
+      description: 'Select the most relevant category for this post.',
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+      group: 'editorialWorkflow',
+    },
+    {
+      name: "author",
+      title: "Author",
+      description: "The author of the post.",
+      type: "reference",
+      to: { type: "author" },
+      group: 'editorialWorkflow',
+    },
+    {
+      name: "featuredImage",
+      title: "Featured image",
+      description: "The really big image at the top of every post. You can add your own from Stable Diffusion or use the built-in Unsplash integration.",
+      type: "image",
+      group: 'meta',
+      fields: [
+        {
+          name: "caption",
+          type: "string",
+          title: "Image caption",
+          description: "Use this to add your Stable Diffusion prompt or attribute a source.",
+        },
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+          description: "E.g. A grumpy looking bald man on a chair."
+        }
+      ],
+      options: {
+        hotspot: true
       }
-    ],
-    options: {
-      hotspot: true
-    }
-  },
-  {
-    name: 'tldr',
-    title: 'TLDR',
-    description: 'The TL;DR of the post. Not for SEO',
-    type: 'array',
-    of: [{ type: 'block' }],
-    options: { maxLength: 300, spellcheck: true },
-  },
-  {
-    name: 'content',
-    title: 'Content',
-    description: 'This is the primary content of the post.',
-    type: 'array',
-    of: [
-      {
-        title: 'Block',
-        type: 'block',
-        styles: [
-          { title: 'Normal', value: 'normal' },
-          { title: 'H2', value: 'h2' },
-          { title: 'H3', value: 'h3' },
-          { title: 'H4', value: 'h4' },
-          { title: 'Quote', value: 'blockquote' },
-        ],
-        lists: [
-          { title: 'Bullet', value: 'bullet' },
-          { title: 'Number', value: 'number' },
-        ],
-        marks: {
-          decorators: [
-            { title: 'Strong', value: 'strong' },
-            { title: 'Code', value: 'code' },
-            { title: 'Strike', value: 'strike-through' },
+    },
+    {
+      name: 'tldr',
+      title: 'TLDR',
+      description: 'The TL;DR of the post. Not for SEO',
+      type: 'array',
+      of: [{ type: 'block' }],
+      options: { maxLength: 300, spellcheck: true },
+      group: 'writing',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      description: 'This is the primary content of the post.',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [
+            { title: 'Normal', value: 'normal' },
+            { title: 'H2', value: 'h2' },
+            { title: 'H3', value: 'h3' },
+            { title: 'H4', value: 'h4' },
+            { title: 'Quote', value: 'blockquote' },
           ],
-          annotations: [
+          lists: [
+            { title: 'Bullet', value: 'bullet' },
+            { title: 'Number', value: 'number' },
+          ],
+          marks: {
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Code', value: 'code' },
+              { title: 'Strike', value: 'strike-through' },
+            ],
+            annotations: [
+              {
+                name: 'link',
+                type: 'object',
+                title: 'URL',
+                fields: [
+                  {
+                    title: 'URL',
+                    name: 'href',
+                    type: 'url',
+                    validation: (Rule: any) =>
+                      Rule.uri({
+                        allowRelative: true,
+                        scheme: ['https', 'http', 'mailto', 'tel'],
+                      }),
+                  },
+                  {
+                    title: 'Open in new tab',
+                    name: 'blank',
+                    type: 'boolean',
+                  },
+                ],
+              },
+            ],
+          },
+        },
+        {
+          type: 'image',
+          options: { hotspot: true },
+          fields: [
             {
-              name: 'link',
-              type: 'object',
-              title: 'URL',
-              fields: [
-                {
-                  title: 'URL',
-                  name: 'href',
-                  type: 'url',
-                  validation: (Rule: any) =>
-                    Rule.uri({
-                      allowRelative: true,
-                      scheme: ['https', 'http', 'mailto', 'tel'],
-                    }),
-                },
-                {
-                  title: 'Open in new tab',
-                  name: 'blank',
-                  type: 'boolean',
-                },
-              ],
+              name: 'alt',
+              type: 'string',
+              title: 'Alternative text',
+              validation: (Rule: any) => Rule.required(),
+            },
+            {
+              name: 'caption',
+              type: 'string',
+              title: 'Caption',
             },
           ],
         },
-      },
-      {
-        type: 'image',
-        options: { hotspot: true },
-        fields: [
-          {
-            name: 'alt',
-            type: 'string',
-            title: 'Alternative text',
-            validation: (Rule: any) => Rule.required(),
-          },
-          {
-            name: 'caption',
-            type: 'string',
-            title: 'Caption',
-          },
-        ],
-      },
-    ],
-    options: { spellcheck: true },
-  },
+      ],
+      options: { spellcheck: true },
+      group: 'writing',
+    },
   ]
 };
 
--- sanity/schemas/tool-schema.ts
+++ sanity/schemas/tool-schema.ts
@@ -7,18 +7,69 @@ const tool = {
     title: 'Tools',
     icon: FaTools,
     type: 'document',
+    groups: [
+        {
+            name: 'partnerInfo',
+            title: 'Partner Info',
+            default: true,
+        },
+        {
+            name: 'toolInfo',
+            title: 'Tool Info',
+        },
+        {
+            name: 'meta',
+            title: 'Meta',
+        },
+        {
+            name: 'seo',
+            title: 'SEO',
+        },
+    ],
     fields: [
+        {
+            name: 'isPartner',
+            title: 'Is Partner',
+            description: "Is the company/maker of the tool a Hey Rebekah partner?",
+            type: 'boolean',
+            group: 'partnerInfo',
+            validation: (Rule: any) => Rule.required(),
+        },
+        {
+            name: 'companyName',
+            title: 'Company',
+            description: 'The name of the company that makes the tool. i.e. BRIL.LA, LLC.',
+            type: 'string',
+            group: 'partnerInfo',
+            validation: (Rule: any) => Rule.required(),
+        },
+        {
+            name: 'partnerContactName',
+            title: 'Partner Contact',
+            description: 'What\'s the name of the contact person at the Partner Company?',
+            type: 'string',
+            group: 'partnerInfo',
+        },
+        {
+            name: 'discount',
+            title: 'Reader Discount',
+            description: 'What\'s the discount for Hey Rebekah readers? i.e. 15% off all plans.',
+            type: 'string',
+            group: 'partnerInfo',
+        },
         {
             name: 'name',
             title: 'Name',
             description: 'Title of the tool',
             type: 'string',
+            group: 'toolInfo',
             validation: (Rule: any) => Rule.required(),
         },
         {
             name: 'slug',
             title: 'Slug',
             type: 'slug',
+            group: 'meta',
             options: {
                 source: 'name',
                 maxLength: 96,
@@ -26,17 +77,11 @@ const tool = {
             validation: (Rule: any) => Rule.required(),
         },
         {
-            name: 'isPartner',
-            title: 'Is Partner',
-            description: "Is the company/maker of the tool a Hey Rebekah partner?",
-            type: 'boolean',
-            validation: (Rule: any) => Rule.required(),
-        },
-        {
             name: 'category',
             title: 'Category',
             type: 'reference',
             to: [{ type: 'category' }],
+            group: 'meta',
             validation: (Rule: any) => Rule.required(),
         },
         {
@@ -44,6 +89,7 @@ const tool = {
             title: "Partner's Logo",
             description: "Upload the partner's logo in SVG format with a transparent background.",
             type: 'image',
+            group: 'toolInfo',
             options: {
                 hotspot: true,
             },
@@ -54,6 +100,7 @@ const tool = {
             title: 'Image Alt Text',
             description: "Enter the alternative text for the partner's logo Image per A11y.",
             type: 'string',
+            group: 'toolInfo',
             validation: (Rule: any) => Rule.required(),
         },
         {
@@ -61,6 +108,7 @@ const tool = {
             title: 'Short Description',
             description: 'Write a brief description of the tool. Like super brief.',
             type: 'string',
+            group: 'toolInfo',
             validation: (Rule: any) => Rule.required(),
         },
         {
@@ -70,12 +118,14 @@ const tool = {
             type: 'array',
             of: [{ type: 'block' }],
             options: { maxLength: 300, spellcheck: true },
+            group: 'toolInfo',
             validation: (Rule: any) => Rule.required(),
         },
         {
             name: 'publishedAt',
             title: 'Published At',
             type: 'date',
+            group: 'meta',
         },
     ],
 };
