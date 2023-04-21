145c782c67f2d85596e293419bceb0e692203b9f
Sam Rizvi <srizvi@users.noreply.github.com>
2023-04-21 00:10:53 -0700

    - Added deskStructure.ts (#9)
    - Optimized Desk Tool nav
    - Need to review Desk Settings with @surjithctly

---
+++ src/deskStructure.ts
@@ -0,0 +1,37 @@
+import { FaArchive, FaBook, FaFeatherAlt, FaQuestion, FaThList, FaTools, FaUserAstronaut } from "react-icons/fa";
+
+export const structure = (S) =>
+    S.list()
+        .title('Let\'s make some magic')
+        .items([
+            S.listItem()
+                .title('Posts')
+                .icon(FaFeatherAlt)
+                .child(S.documentTypeList('gist')),
+            S.listItem()
+                .title('Books')
+                .icon(FaBook)
+                .child(S.documentTypeList('book')),
+            S.listItem()
+                .title('Tools')
+                .icon(FaTools)
+                .child(S.documentTypeList('tool')),
+            S.divider(),
+            S.listItem()
+                .title('Topics')
+                .icon(FaThList)
+                .child(S.documentTypeList('category')),
+            S.listItem()
+                .title('Authors')
+                .icon(FaUserAstronaut)
+                .child(S.documentTypeList('author')),
+            S.divider(),
+            S.listItem()
+                .title('FAQs')
+                .icon(FaQuestion)
+                .child(S.documentTypeList('faq')),
+            S.listItem()
+                .title('Rebekah\'s Old Blogs')
+                .icon(FaArchive)
+                .child(S.documentTypeList('socialBlog'))
+        ]);
