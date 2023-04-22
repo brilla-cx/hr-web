5ac80164efafdcdb6c420d1a27a1b912991b7102
Surjith S M <surjithctly@gmail.com>
2023-04-22 15:13:41 +0530

    fix build errors

--- components/postalt.js
+++ components/postalt.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { cx } from "@/utils/all";
+import { cx } from "@/lib/utils";
 import { urlForImage } from "@/sanity/image";
 import { parseISO, format } from "date-fns";
 import { PhotoIcon } from "@heroicons/react/24/outline";
