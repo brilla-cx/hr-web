109b0103ac6c5d73791b3031f5a9496c2a401f09
Sam Rizvi <srizvi@users.noreply.github.com>
2023-05-02 04:33:15 -0700

    Updated Home Page Styles (#30)
    - Changed BG color to dark blue
    - Updated typography colors
    - Minor copy updates
    - Added bg-white and changed hover shadow to yellow on card comp
    - Added h-16 to headings for consistency
    - Close T-10033

--- components/ui/card.js
+++ components/ui/card.js
@@ -5,7 +5,7 @@ export default function Card({ children, className = "", ...rest }) {
     <div
       className={cx(
         className,
-        "isolate rounded-lg overflow-hidden  border-2 border-black relative transition-all hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[5px_5px_0_rgb(41,37,36)] hover:after:absolute hover:after:top-0 hover:after:left-0 hover:after:-bottom-2 hover:after:-right-2 hover:after:-z-10"
+        "isolate rounded-lg overflow-hidden  border-2 border-black relative transition-all hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[5px_5px_0_rgb(255,207,51)] hover:after:absolute hover:after:top-0 hover:after:left-0 hover:after:-bottom-2 hover:after:-right-2 hover:after:-z-10"
       )}
       {...rest}>
       {children}
