44130e64cd527e5f27e7d6f36b05fe8d8ac7d60c
Sam Rizvi <srizvi@users.noreply.github.com>
2023-05-02 04:29:15 -0700

    Updated Home Page Styles - Changed BG color to dark blue - Updated typography colors - Minor copy updates - Added bg-white and changed hover shadow to yellow on card comp - Added h-16 to headings for consistency - Close T-10033

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -13,56 +13,57 @@ export default function HomePage({ posts }) {
           <Hero />
         </Container>
       </div>
-
-      <Container large>
-        {featuredPost.length > 4 && (
-          <>
-            <div className="flex items-center justify-center mt-10">
-              <H2>
-                <span className="text-pink">Featured</span> Posts
-              </H2>
-            </div>
-            <div className="grid gap-10 mt-10 mb-20 lg:gap-10 md:grid-cols-3 lg:grid-cols-4 ">
-              {featuredPost.slice(1, 2).map((post) => (
-                <div className="md:col-span-2 md:row-span-2" key={post._id}>
+      <div className="bg-dark-blue">
+        <Container large>
+          {featuredPost.length > 4 && (
+            <>
+              <div className="flex items-center justify-center mt-10">
+                <H2 className="text-white">
+                  <span className="text-pink">🔥 Most</span> Popular
+                </H2>
+              </div>
+              <div className="grid gap-10 mt-10 mb-20 lg:gap-10 md:grid-cols-3 lg:grid-cols-4 ">
+                {featuredPost.slice(1, 2).map((post) => (
+                  <div className="md:col-span-2 md:row-span-2" key={post._id}>
+                    <PostList
+                      post={post}
+                      preloadImage={true}
+                      fontSize="large"
+                      aspect="custom"
+                      fontWeight="normal"
+                    />
+                  </div>
+                ))}
+                {featuredPost.slice(2, 6).map((post) => (
                   <PostList
+                    key={post._id}
                     post={post}
-                    preloadImage={true}
-                    fontSize="large"
-                    aspect="custom"
+                    aspect="landscape"
                     fontWeight="normal"
+                    preloadImage={true}
                   />
-                </div>
-              ))}
-              {featuredPost.slice(2, 6).map((post) => (
-                <PostList
-                  key={post._id}
-                  post={post}
-                  aspect="landscape"
-                  fontWeight="normal"
-                  preloadImage={true}
-                />
-              ))}
-            </div>
-          </>
-        )}
+                ))}
+              </div>
+            </>
+          )}
 
-        <div className="flex items-center justify-center mt-4">
-          <H3>
-            Our <span className="text-pink">Latest</span>
-          </H3>
-        </div>
-        <div className="grid gap-10 mt-10 lg:gap-10 md:grid-cols-2 xl:grid-cols-4 ">
-          {posts.slice(0, 12).map((post) => (
-            <PostList
-              key={post._id}
-              post={post}
-              fontWeight="normal"
-              aspect="square"
-            />
-          ))}
-        </div>
-      </Container>
+          <div className="flex items-center justify-center mt-4">
+            <H2 className="text-white">
+              🫳🏼 Just <span className="text-pink">Dropped</span>
+            </H2>
+          </div>
+          <div className="grid gap-10 mt-10 lg:gap-10 md:grid-cols-2 xl:grid-cols-4 ">
+            {posts.slice(0, 12).map((post) => (
+              <PostList
+                key={post._id}
+                post={post}
+                fontWeight="normal"
+                aspect="square"
+              />
+            ))}
+          </div>
+        </Container>
+      </div>
     </>
   );
 }
