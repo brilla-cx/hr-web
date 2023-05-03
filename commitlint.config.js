09e587528a944d59d2861d2cad7927fd4185fd06
srizvi <srizvi@users.noreply.github.com>
2023-05-03 05:39:28 -0700

    chore(lint): Update dev env for signoff
    - add auto-signoff to git commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,21 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 72],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      ["app", "server", "client", "database", "docs", "tests", "lint"],
+    ],
+    "subject-case": [2, "always", "sentence-case"],
+    "body-max-line-length": [2, "always", 72],
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+    "signed-off-by": [2, "always"],
+  },
+};
