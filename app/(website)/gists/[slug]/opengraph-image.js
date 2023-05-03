fc7d7f9494a5838130e422099af376157aae357c
Surjith S M <surjithctly@gmail.com>
2023-05-03 21:23:13 +0530

    feat: add open graph image

---
+++ app/(website)/gists/[slug]/opengraph-image.js
@@ -0,0 +1,35 @@
+import { ImageResponse } from "@vercel/og";
+import { getPostBySlug } from "@/sanity/client";
+import OgImage from "@/components/blog/ogimage";
+
+export const runtime = "edge";
+
+export default async function handler({ params }) {
+  const post = await getPostBySlug(params.slug);
+  const apfelGrotezk = fetch(
+    new URL("../../assets/fonts/apfel-grotezk-fett.otf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+  const fontData = await apfelGrotezk;
+  // const [interRegularFont, interBoldFont] = await Promise.all([
+  //   InterRegular,
+  //   InterBold
+  // ]);
+
+  return new ImageResponse(<OgImage post={post} />, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      {
+        name: "Apfel",
+        data: fontData,
+        style: "normal",
+      },
+      // {
+      //   name: "Inter",
+      //   data: interBoldFont,
+      //   style: "normal",
+      //   weight: 700
+      // }
+    ],
+  });
+}
