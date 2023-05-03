d995bd54952a856306407ae303a347a7e01257fc
Sam Rizvi <srizvi@users.noreply.github.com>
2023-05-03 06:10:36 -0700

    T 9698 (#34)
    * feat: release optimization
    - Implement and configure Commitzen
    - Implement and configure Husky with @commitlint/cli
    - Implement and configure Standard Version (semver)
    - Update package manager
    
    * feat: implement standard version
    - Added Standard Version
    - Updated package.json with scipt
    
    * chore(lint): Update dev env for signoff
    
    - add auto-signoff to git commits
    
    * docs: Update README.md
    
    - Add linting rules and config to README

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 72],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      ["app", "server", "client", "database", "docs", "tests", "lint"],
+    ],
+    "subject-case": [2, "always", "sentence-case"],
+    "body-max-line-length": [2, "always", 72],
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+  },
+};
