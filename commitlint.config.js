450877aafbec4b52e7e9d41c6b3127602f7fe04d
srizvi <srizvi@users.noreply.github.com>
2023-05-03 23:59:22 -0700

    feat(lint): Add Height Task ID
    - Add linting rules to ensure Height Task ID present in commit msg body
    - T-10158

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,25 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  plugins: [
+    {
+      rules: {
+        "height-task-number": (parsed) => {
+          const { header, body, footer } = parsed;
+          const regex = /(Close\s|Link\s)?T-\d{1,7}/;
+          const isValid =
+            regex.test(header) || regex.test(body) || regex.test(footer);
+
+          if (!isValid) {
+            return [
+              false,
+              "Message must include a Height Task number in the format 'T-#######', 'Close T-#######', or 'Link T-#######'",
+            ];
+          }
+          return [true, ""];
+        },
+      },
+    },
+  ],
   rules: {
     "header-max-length": [2, "always", 72],
     "type-enum": [
@@ -13,8 +33,9 @@ module.exports = {
       ["app", "server", "client", "database", "docs", "tests", "lint"],
     ],
     "subject-case": [2, "always", "sentence-case"],
-    "body-max-line-length": [2, "always", 72],
+    "body-max-line-length": [2, "always", Infinity],
     "body-leading-blank": [2, "always"],
+    "height-task-number": [2, "always"],
     "footer-leading-blank": [2, "always"],
   },
 };
