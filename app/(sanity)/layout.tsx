48f5d34e0f9be0dbad17c2140028e3a672832414
Surjith S M <surjithctly@gmail.com>
2023-05-15 18:56:10 +0530

    Revert "test: remove sanity"
    This reverts commit e400a8e1bfab6b3e50be38816355472df7c0db39.

---
+++ app/(sanity)/layout.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import "./reset.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/(sanity)/studio/[[...index]]/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { NextStudio } from 'next-sanity/studio';
+
+import config from '@/sanity.config';
+
+
+export default function AdminPage() {
+  return <NextStudio config={config} />
+}
