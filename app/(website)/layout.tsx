a2aa1d08259a280981ba4a507fb3c7a65b8476db
Surjith S M <surjithctly@gmail.com>
2023-05-17 13:40:41 +0530

    fix: async build error

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -55,6 +55,7 @@ export default function RootLayout({
         apfelGrotezk.variable
       )}>
       <body>
+        {/* @ts-expect-error Server Component */}
         <Navbar />
         <div>{children}</div>
         <Footer />
