b157ab9d464e777a6ed93552e1ef7e0105db2d29
Sam Rizvi <srizvi@users.noreply.github.com>
2023-05-19 19:33:13 -0700

    chore(layout.tsx): Add Metadata API props (#71)
    - chore: update website favicon and icons
    - chore: add web app manifest to website
    - chore: Close T-10842, Close T-10849
    - chore: removed metadata schema
    - chore: add seo-schema object schema to post-schema.ts

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -4,14 +4,8 @@ import book from "./book-schema";
 import category from "./category-schema";
 import faq from "./faq-schema";
 import legal from "./legal-schema";
-import metadata from "./metadata-schema";
 import blockContent from "./objects/blockContent";
-import ldjson from "./objects/ldjson-meta";
-import ogMeta from "./objects/og-meta";
-import pinterestMeta from "./objects/pinterest-meta";
 import seo from "./objects/seo-schema";
-import siteMeta from "./objects/site-meta";
-import twitterMeta from "./objects/twitter-meta";
 import post from "./post-schema";
 import quiz from "./quiz-schema";
 import quote from "./quote-schema";
@@ -25,18 +19,12 @@ const schemas = [
   category,
   faq,
   legal,
-  ldjson,
-  metadata,
-  ogMeta,
-  pinterestMeta,
   post,
   quiz,
   quote,
   seo,
-  siteMeta,
   socialBlog,
   tool,
-  twitterMeta
 ];
 
 export default schemas;
