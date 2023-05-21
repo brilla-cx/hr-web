b0987bb9f0cb02d29015c6d46bb8f690039a84fe
srizvi <srizvi@users.noreply.github.com>
2023-05-20 21:52:21 -0700

    style: Additional global style updates
    - style: Updated text colors and link styles authorCard.js
    - style: Added hover and transform scale effects to SocialShare
    - chore: Added aria labels to SocialShare
    - style: Updated menu styles and colors
    - chore: T-10858

--- components/blog/authorCard.js
+++ components/blog/authorCard.js
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 import { PortableText } from "@/components/blog/portabletext";
 import { H6 } from "@/components/ui";
+import hoverStyles from "@/lib/hover";
+import { cx } from "@/lib/utils";
 import { urlForImage } from "@/sanity/image";
 
 export default function AuthorCard({ author }) {
   const imageProps = author?.image ? urlForImage(author.image) : null;
   return (
-    <div className="px-8 py-8 mt-3 text-gray-600 rounded-2xl bg-gray-100 max-w-2xl mx-auto">
+    <div className="px-8 py-8 mt-3 text-slate-600 rounded-2xl bg-gray-100 max-w-2xl mx-auto">
       <div className="flex flex-wrap items-start sm:space-x-6 sm:flex-nowrap">
         <div className="relative flex-shrink-0 w-24 h-24 mt-1 ">
           {imageProps && (
@@ -24,18 +26,22 @@ export default function AuthorCard({ author }) {
           )}
         </div>
         <div>
-          <div className="my-3 md:mt-0">
-            <H6 as="h3" className="text-lg font-medium text-gray-800">
+          <div className="my-5 md:mt-0">
+            <H6 as="h3" className="text-lg font-medium text-slate-900">
               About {author.name}
             </H6>
           </div>
-          <div className="line-clamp-3">
+          <div className="line-clamp-3 text-slate-600 mb-2">
             {author.bio && <PortableText value={author.bio} />}
           </div>
-          <div className="mt-3">
+          <div className="mt-3 inline">
             <Link
               href={`/author/${author.slug.current}`}
-              className="text-sm text-indigo-950 underline decoration-pink decoration-2 underline-offset-4 hover:text-pink hover:underline hover:underline-offset-4 hover:decoration-indigo-950 hover:decoration-2 transition-all duration-200 ">
+              className={cx(
+                "text-sm text-slate-900 font-bold decoration-none",
+                hoverStyles
+              )}
+              aria-label="View author profile">
               View Profile
             </Link>
           </div>
