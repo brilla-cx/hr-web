79c1211a36f6d36b4af191930edd4a531027bc30
Sam Rizvi <srizvi@users.noreply.github.com>
2023-06-01 22:31:23 -0700

    - chore(package.json): fix pnpm package manager issues - feat(deskStructure.ts): add count of documents to each list item in the desk structure Close T-9537 - chore: update desk structure labels

--- sanity/settings/deskStructure.ts
+++ sanity/settings/deskStructure.ts
@@ -2,6 +2,7 @@ import {
   FaArchive,
   FaBook,
   FaFeatherAlt,
+  FaFileAlt,
   FaListUl,
   FaQuestion,
   FaQuoteLeft,
@@ -9,62 +10,165 @@ import {
   FaTools,
   FaUserAstronaut,
 } from "react-icons/fa";
-import { GoLaw } from "react-icons/go";
+import { map } from 'rxjs/operators';
 import { DefaultDocumentNodeResolver } from "sanity/desk";
 import Iframe from 'sanity-plugin-iframe-pane'
 
 import { SITE_URL } from "../../lib/constants";
 // import { SEOPane } from "sanity-plugin-seo-pane";
 
-export const structure = (S) =>
-  S.list()
+export const structure = (S, context) => {
+  const { documentStore } = context;
+
+  return S.list()
     .title("Let's make some magic")
     .items([
       S.listItem()
         .title("Posts")
         .icon(FaFeatherAlt)
-        .child(S.documentTypeList("post")),
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'post'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('post')
+                  .title(`Posts (${count})`)
+                  .filter('_type == "post"')
+              )
+            )
+        ),
       S.listItem()
         .title("Books")
         .icon(FaBook)
-        .child(S.documentTypeList("book")),
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'book'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('book')
+                  .title(`Books (${count})`)
+                  .filter('_type == "book"')
+              )
+            )
+        ),
       S.listItem()
         .title("Tools")
         .icon(FaTools)
-        .child(S.documentTypeList("tool")),
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'tool'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('tool')
+                  .title(`Built With Tools (${count})`)
+                  .filter('_type == "tool"')
+              )
+            )
+        ),
       S.divider(),
       S.listItem()
-        .title("Topics")
+        .title("Categories")
         .icon(FaThList)
-        .child(S.documentTypeList("category")),
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'category'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('category')
+                  .title(`Categories (${count})`)
+                  .filter('_type == "category"')
+              )
+            )
+        ),
       S.listItem()
         .title("Authors")
         .icon(FaUserAstronaut)
-        .child(S.documentTypeList("author")),
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'author'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('author')
+                  .title(`Authors (${count})`)
+                  .filter('_type == "author"')
+              )
+            )
+        ),
       S.divider(),
       S.listItem()
         .title("FAQs")
         .icon(FaQuestion)
-        .child(S.documentTypeList("faq")),
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'faq'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('faq')
+                  .title(`FAQ (${count})`)
+                  .filter('_type == "faq"')
+              )
+            )
+        ),
       S.listItem()
-        .title("Quiz Questions")
+        .title("Quizesseses")
         .icon(FaListUl)
-        .child(S.documentTypeList("quiz")),
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'quiz'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('quiz')
+                  .title(`Quiz (${count})`)
+                  .filter('_type == "quiz"')
+              )
+            )
+        ),
       S.listItem()
         .title("Quotes")
         .icon(FaQuoteLeft)
-        .child(S.documentTypeList("quote")),
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'quote'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('quote')
+                  .title(`Quotes (${count})`)
+                  .filter('_type == "quote"')
+              )
+            )
+        ),
       S.divider(),
       S.listItem()
-        .title("Privacy & Terms")
-        .icon(GoLaw)
-        .child(S.documentTypeList("legal")),
+        .title("Legal Pages")
+        .icon(FaFileAlt)
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'legal'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('legal')
+                  .title(`Legal Pages (${count})`)
+                  .filter('_type == "legal"')
+              )
+            )
+        ),
       S.divider(),
       S.listItem()
         .title("Rebekah's Old Blogs")
         .icon(FaArchive)
-        .child(S.documentTypeList("socialBlog")),
+        .child(
+          documentStore
+            .listenQuery(`count(*[_type == 'socialBlog'])`)
+            .pipe(
+              map(count =>
+                S.documentTypeList('socialBlog')
+                  .title(`Social Blogs (${count})`)
+                  .filter('_type == "socialBlog"')
+              )
+            )
+        ),
     ]);
+};
 
 function getPreviewUrl(doc) {
   const absoluteURL =
