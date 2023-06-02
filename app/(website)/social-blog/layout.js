b59bbb4d59ca06dff16ae0cc34b70ac8a90e993e
Sam Rizvi <srizvi@users.noreply.github.com>
2023-06-02 01:14:15 -0700

    - refactor(deskStructure.ts): create a reusable function to generate list items for document types and use it to generate all list items in the desk structure. - refactor(SocialBlogs): renamed socialblogs.js to layout for maintainability - chore: remove axe-linter.yml and `next.lock` folder used for Sanity themer

---
+++ app/(website)/social-blog/layout.js
@@ -0,0 +1,31 @@
+import { Suspense } from "react";
+
+import Container from "@/components/container";
+import PageHeader from "@/components/ui/sections/pageheader";
+
+import PaginatedPosts from "./posts";
+
+export default function SocialBlogs() {
+  return (
+    <div className="bg-midnight">
+      <Container
+        large
+        className="border-l border-r border-neutral-200 border-opacity-10">
+        <PageHeader
+          title="Social Media Blog"
+          leadText="Since launching in 2004, over 24 million unique visitors have read my posts, and no, it's not just my mom on a refresh marathon."
+          includeForm
+          formId="socialblog-sub"
+        />
+        <div className="mx-auto max-w-6xl px-4 text-gray-200">
+          <Suspense fallback={<p>Walking the dead...</p>}>
+            <PaginatedPosts />
+          </Suspense>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+// Export revalidate for controlling data fetching behavior
+export const revalidate = 3600;
