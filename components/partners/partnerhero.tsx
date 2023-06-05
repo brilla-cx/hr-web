42db8df0ada4017b48eb01cb4f2507d8b9b3f131
Sam Rizvi <srizvi@users.noreply.github.com>
2023-06-05 08:51:41 -0700

    - refactor(partners): rename layout.tsx to partnerhero.tsx - feat(partners): add PageHeader component to Partners page - refactor(social-blog): rename layout.js to socialblogs.js - refactor(partnerhero.tsx): add alt, imageWidth, and imageHeight props to PartnerHero component

--- components/partners/partnerhero.tsx
+++ components/partners/partnerhero.tsx
@@ -2,38 +2,37 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+import { H1 } from '../ui';
+
 interface PartnerHeroProps {
     title: string;
     description: string;
+    alt: string;
     imageUrl: string;
+    imageWidth: number;
+    imageHeight: number;
+
 }
 
 const PartnerHero: React.FC<PartnerHeroProps> = ({
     title,
     description,
+    alt,
     imageUrl,
+    imageWidth,
+    imageHeight,
 }) => {
     return (
-        <div className="bg-gray-900">
-            <div className="relative isolate overflow-hidden pt-14">
+        <div className="bg-midnight h-screen">
+            <div className="relative h-full isolate overflow-hidden pt-14">
                 <Image
                     src={imageUrl}
-                    alt=""
+                    alt={alt}
                     className="absolute inset-0 -z-10 h-full w-full object-cover"
+                    width={imageWidth}
+                    height={imageHeight}
                 />
-                <div
-                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-                    aria-hidden="true"
-                >
-                    <div
-                        className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
-                    />
-                </div>
-                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                <div className="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56 items-center justify-center">
                     <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                         <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
                             Announcing our next round of funding.{" "}
@@ -44,36 +43,21 @@ const PartnerHero: React.FC<PartnerHeroProps> = ({
                         </div>
                     </div>
                     <div className="text-center">
-                        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                        <H1 className=" text-white">
                             {title}
-                        </h1>
+                        </H1>
                         <p className="mt-6 text-lg leading-8 text-gray-300">
                             {description}
                         </p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
                             <Link
                                 href="#"
-                                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                                className="rounded bg-pink px-3.5 py-2.5 uppercase font-semibold text-midnight shadow-sm hover:bg-slate-900 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-200/10"
                             >
-                                Get started
-                            </Link>
-                            <Link href="#" className="text-sm font-semibold leading-6 text-white">
-                                Learn more <span aria-hidden="true">→</span>
+                                Become a Partner
                             </Link>
                         </div>
                     </div>
-                </div>
-                <div
-                    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-                    aria-hidden="true"
-                >
-                    <div
-                        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
-                    />
                 </div>
             </div>
         </div>
