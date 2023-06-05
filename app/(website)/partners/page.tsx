8e472189ca1ac923de6ca1a8a6c5815b352ed2e3
Sam Rizvi <srizvi@users.noreply.github.com>
2023-06-05 08:14:45 -0700

    - feat(partners): add PartnerHero component with title, description, and imageUrl props. - chore: update title and description props

--- app/(website)/partners/page.tsx
+++ app/(website)/partners/page.tsx
@@ -4,7 +4,10 @@ export default function Partners() {
   //Placeholder to create route
   return (
     <div>
-      <PartnerHero title={""} description={""} imageUrl={""} />
+      <PartnerHero
+        title={"Partner with Hey Rebekah"}
+        description={"Improve your good karma and do a solid for the 420 million freelancers around the world. Instead of paying us a fat affiliate commission, we ask that you provide an exclusive discount to our readers. No brainer, right?"}
+        imageUrl={""} />
     </div>
 
   );
