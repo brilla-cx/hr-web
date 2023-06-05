d64d5229718986413e9e2acae04f7379eb6679e7
Surjith S M <surjithctly@gmail.com>
2023-06-05 20:26:53 +0530

    fix: build error

--- components/blog/category.js
+++ components/blog/category.js
@@ -4,14 +4,16 @@ import Label from "@/components/ui/label";
 
 export default function CategoryLabel({ categories, nomargin = false }) {
   const categoryArray = Array.isArray(categories) ? categories : [categories];
-
+  console.log(categoryArray);
   return (
     <div className="flex gap-3">
       {categoryArray?.length &&
         categoryArray.slice(0).map((category) => (
-          <Link href={`/category/${category.slug.current}`} key={category._id}>
-            <Label nomargin={nomargin} color={category.color}>
-              {category.name}
+          <Link
+            href={`/category/${category?.slug?.current}`}
+            key={category?._id}>
+            <Label nomargin={nomargin} color={category?.color}>
+              {category?.name}
             </Label>
           </Link>
         ))}
