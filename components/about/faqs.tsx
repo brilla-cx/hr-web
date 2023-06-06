c14f706e0f89baa491e12cc24f81e6886017a18a
samuelwondimu <wsamuelwondimu@gmail.com>
2023-06-06 18:07:45 +0300

    fix: next-13 build error for headless ui 'use client'

--- components/about/faqs.tsx
+++ components/about/faqs.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Disclosure } from "@headlessui/react";
 import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/20/solid";
 import { PortableText } from "@portabletext/react";
