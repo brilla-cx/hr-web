d7b72319233ce0002e7f085d4b8f8f4b2cf70e4b
Sam Rizvi <srizvi@users.noreply.github.com>
2023-06-09 05:04:23 -0700

    - refactor(about): rename Hero component to HeroWithImage - refactor(herowithimage): add GlowingButton component to HeroWithImage - refactor(wtf-is-rebekah): change Lead text to better reflect Rebekah's mission and add a blockquote - style(herowithimage): adjust padding and spacing in HeroWithImage component

--- components/about/wtf-is-rebekah.tsx
+++ components/about/wtf-is-rebekah.tsx
@@ -1,44 +1,44 @@
 import Link from "next/link";
-import React from "react";
+import React, { FC } from "react";
 
 import hoverStyles from "@/lib/hover";
 import { cx } from "@/lib/utils";
 
-import { H1, Lead } from "../ui";
+import { H3, Lead } from "../ui";
 
-function WtfIsHeyRebekah() {
+const WtfIsHeyRebekah: FC = () => {
   return (
-    <div className="p-4 mt-10 text-center border rounded border-gray-200/10 bg-slate-900 md:p-20 lg:mt-0 lg:p-28">
-      <H1 as="h3" className="pb-6 text-gray-200">
+    <div className="bg-midnight">
+      <H3 as="h2" className="py-6 text-gray-200 text-center">
         Who TF is Rebekah Radice?
-      </H1>
-      <div className="px-2 space-y-6 text-lg text-gray-400 md:px-20 lg:px-28">
+      </H3>
+      <div className="mx-auto px-2 space-y-6 text-xl text-gray-400 max-w-2xl justify-center">
         <Lead className="gap-7">
-          According to the interwebs, Rebekah Radice is one of the top digital
-          marketing experts in the world. She's an OG influencer and a
-          self-employed professional since 1998.
+          Rebekah Radice helps ambitious professionals thrive in today's world of work.
         </Lead>
-        <Lead>
-          In 2019, she started{" "}
+        <Lead className="gap-7">
+          For 45+ years, she's shown career-driven visionaries how to navigate constant change.
+        </Lead>
+        <Lead className="gap-7">
+          Now the co-founder of{" "}
           <Link
             href="https://bril.la/"
-            className={cx(hoverStyles, "pr-1 font-bold text-gray-200")}>
+            className={cx(hoverStyles, "pr-1 font-bold text-gray-100")}>
             BRIL.LA
           </Link>
-          , a woman-owned CX agency with her longtime colleague Ambreen Dar—who
-          prefers protegé, not sidekick.
+          , a woman-owned agency, she launched Hey Rebekah with her longtime colleague, Ambreen Dar. The newsletter offers simple tips to help readers adapt to new technology and build careers with purpose.
         </Lead>
-        <Lead>
-          Hey Rebekah came about after receiving thousands of questions from the
-          community. We realized we could do a lot more to help self-employed
-          professionals everywhere.
+        <Lead className="gap-7">
+          Together, they help ambitious professionals thrive in today's world of work. After years empowering people with purpose, they believe this:
+          <blockquote className="mt-8 mx-4 my-7 p-6 pl-4 border-l-4 border-pink text-gray-300 text-2xl leading-loose">
+            We all long to push past limits into uncharted territory. Technology may change how we work, but it can't replace our thirst for creativity, meaning, and impact.
+          </blockquote>
         </Lead>
-
-        <Lead>
-          We're starting with a free daily newsletter to do just that. Together,
-          we're on a mission to help freelance professionals build thriving
-          careers, so they can experience the joy of financial freedom and
-          success.
+        <Lead className="gap-7">
+          With Hey Rebekah, Rebekah and Ambreen aim to inspire that longing in knowledge professionals. They stoke dreams as bold as horizons. And fuel readers daily with wisdom and tools to turn visions into reality—and find success, joy, and lifelong purpose along the journey.
+        </Lead>
+        <Lead className="gap-7 pb-6">
+          In a world of unknowns, Rebekah and Ambreen light the way. Their newsletter opens minds to new horizons of what's possible, achievable, and meaningful in our careers. For those with ambition, passion and an appetite for growth, the voyage awaits.
         </Lead>
       </div>
     </div>
---
+++ components/sections/herowithimage.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+
+import { GlowingButton, H1, Lead } from "@/components/ui";
+
+
+interface Props {
+  title: string;
+  subtitle: string;
+  subtitle2?: string;
+  image: string;
+  button?: JSX.Element;
+}
+
+export function HeroWithImage(props: Props) {
+  const { image, subtitle, subtitle2, title } = props;
+  return (
+    <div className="grid items-center grid-cols-2 gap-4 lg:py-26 px-4 py-12 sm:px-8 sm:py-20 lg:px-16">
+      <div className="hidden md:block lg:col-span-1">
+        <Image
+          className="object-cover w-full h-full"
+          src={image}
+          alt=""
+          width={100}
+          height={100}
+          placeholder="blur"
+          blurDataURL={image}
+          priority
+          unoptimized
+        />
+      </div>
+      <div className="col-span-2 md:col-span-1">
+        <H1 className="text-gray-200">{title}</H1>
+        <Lead className="mt-6 text-gray-400">{subtitle}</Lead>
+        <Lead className="mt-6 text-gray-400 ">{subtitle2}</Lead>
+
+        <div className="pt-6 max-w-xs">{props.button && props.button}
+          button={
+            <GlowingButton
+              variant="link"
+              href="/signup"
+              size="md"
+              id="about-hero"
+            >
+              Join the Curious
+            </GlowingButton>
+          }
+        </div>
+
+      </div>
+    </div>
+  );
+}
