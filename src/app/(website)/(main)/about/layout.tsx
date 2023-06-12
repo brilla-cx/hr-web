5b70ff68ef259db220874f41e16f49883c4d4e3d
samuelwondimu <wsamuelwondimu@gmail.com>
2023-06-12 18:35:44 +0300

    about meta data

--- src/app/(website)/(main)/about/layout.tsx
+++ src/app/(website)/(main)/about/layout.tsx
@@ -2,17 +2,9 @@ import React from "react";
 
 export function generateMetadata() {
   return {
-    title: "About | Knowledge should be free. Experience, priceless.",
+    title: "What is Hey Rebekah? | #1 Newsletter for Freelancing",
     description:
-      "Freelancers need better access to knowledge, skills, and tools to build thriving careers. That's our focus.",
-    twitter: {
-      card: "summary_large_image", // type of card you want to use. It could be "summary", "summary_large_image", "app", or "player"
-      site: "rebekahradice", // your Twitter handle
-      title: "Hey Rebekah | Like Morning Brew for freelancers", // the title of the content
-      description: "Hey Rebekah is a free daily newsletter for freelancers...", // a description of the content
-      creator: "rebekahradice",
-      image: "public/og.png",
-    },
+      "Wondering WTF is Hey Rebekah? LOL, no worries. It's a free daily newsletter produced by Rebekah Radice. It's like Morning Brew for freelancers. 💪🏽",
   };
 }
 
