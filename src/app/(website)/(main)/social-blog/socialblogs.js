8ca4363e44c2be260b235cae825ec774e6606bd0
Sam Rizvi <srizvi@users.noreply.github.com>
2023-06-12 07:21:19 -0700

    - chore: update community numbers in various files - chore: Close T-11935

--- src/app/(website)/(main)/social-blog/socialblogs.js
+++ src/app/(website)/(main)/social-blog/socialblogs.js
@@ -11,7 +11,7 @@ export default function SocialBlogs() {
       <Container large className="border-l border-r border-neutral-200/10">
         <PageHeader
           title="Social Media Blog"
-          leadText="Over 24M readers and counting since 2004. Accidentally actionable, always authentic. Proud member of the unemployables and working hard to make knowledge free for professionals everywhere. Join me and 320,000 members of my community as we learn to harness AI to make our careers more human. 👇🏾"
+          leadText="Over 24M readers and counting since 2004. Accidentally actionable, always authentic. Proud member of the unemployables and working hard to make knowledge free for professionals everywhere. Join me and 338,000 members of my community as we learn to harness AI to make our careers more human. 👇🏾"
           id="sblogs"
           includeForm
         />
