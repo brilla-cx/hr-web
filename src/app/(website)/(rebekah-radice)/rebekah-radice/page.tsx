48608269be42b10b24670a7e6b23e177d3b770aa
samuelwondimu <wsamuelwondimu@gmail.com>
2023-06-13 14:02:36 +0300

    add og image to rebakah-radice page

--- src/app/(website)/(rebekah-radice)/rebekah-radice/page.tsx
+++ src/app/(website)/(rebekah-radice)/rebekah-radice/page.tsx
@@ -11,11 +11,13 @@ export function generateMetadata(): Metadata {
       title: "Rebekah Radice - Social Media Speaker, Consultant, Trainer",
       description:
         "Rebekah Radice is a social media speaker, consultant, and trainer in Los Angeles, CA. Rebekah helps growth-driven leaders build brand awareness and authority.",
+      images: "/og.png",
     },
     twitter: {
       title: "Rebekah Radice - Social Media Speaker, Consultant, Trainer",
       description:
         "Rebekah Radice is a social media speaker, consultant, and trainer in Los Angeles, CA. Rebekah helps growth-driven leaders build brand awareness and authority.",
+      images: "/og.png",
     },
   };
 }
