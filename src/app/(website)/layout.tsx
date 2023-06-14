41ef6d0d5d0f3d5f0e08c1d2c2ef330870a58764
Sam Rizvi <srizvi@users.noreply.github.com>
2023-06-13 17:37:54 -0700

    - feat: add PlausibleProvider to website layout - feat: add next-plausible package to dependencies - feat: add middleware for /proxy/api/event route in middleware.ts to remove proxy cookies - chore: add next-plausible to next.config.js using withPlausibleProxy() - chore: Close T-11940

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -4,6 +4,7 @@ import "../globals.css";
 
 import { Metadata } from "next";
 import localFont from "next/font/local";
+import PlausibleProvider from "next-plausible";
 import React from "react";
 
 import { SITE_URL } from "@/lib/constants";
@@ -137,6 +138,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cx("font-sans antialiased", lexend.variable)}>
+      <head>
+        <PlausibleProvider
+          domain="heyrebekah.com"
+          trackOutboundLinks
+          trackFileDownloads
+          taggedEvents
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
