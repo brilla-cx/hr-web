2b8598a8721ef0bd36d1dcc2c936485cdbb84802
Sam Rizvi <srizvi@users.noreply.github.com>
2023-06-19 05:11:16 -0700

    chore(social-blog, rebekah-radice): Update project structure (#208)
    * chore(social-blog): Update project structure
    
    - organized folder structure to Feature Folder hierarchy
      - moved specific components into components folders
      - nested components into folders for future tests
      - renamed component files to PascalCase and logical names
    - updated import paths to match new file structure
    
    Close T-12248
    
    * chore(rebekah-radice): Update project structure
    
    - organized folder structure to Feature Folder hierarchy
      - moved specific components into components folder within leaf
      - nested components into folders for future tests
      - renamed component files to PascalCase and logical names
    - updated import paths to match new file structure
    - need to refactor @samuelwondimu for App Router
    
    Close T-12250
    
    * chore(rebekah-radice): Update imports
    
    - revert imports to absolute instead of really long relative
    
    Link T-12219

--- src/app/(website)/(main)/social-blog/[slug]/components/Preview/preview.js
+++ src/app/(website)/(main)/social-blog/[slug]/components/Preview/preview.js
@@ -3,7 +3,7 @@
 import { singlesocialblogquery } from "@/sanity/groq";
 import { usePreview } from "@/sanity/preview";
 
-import SocialBlog from "./socialblog";
+import SocialBlog from "../SocialBlog/SocialBlog";
 
 export default function SocialBlogPreview({ slug }) {
   const socialBlog = usePreview(null, singlesocialblogquery, {
