d2d423c8430deb230ea45e5738ac2e3c0e1778ad
Sam Rizvi <srizvi@users.noreply.github.com>
2023-06-20 06:20:59 -0700

    chore(layout.tsx): Rename Navbar component folder
    - renamed Navbar component folder to MainNavbar to fix bug in VS Code
    
    Link T-9299

--- src/app/(website)/(main)/layout.tsx
+++ src/app/(website)/(main)/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Footer from "@/components/layout/Footer";
-import Navbar from "@/components/layout/Navbar";
+import Navbar from "@/components/layout/MainNavbar";
 
 
 function Layout({ children }: { children: React.ReactNode }) {
