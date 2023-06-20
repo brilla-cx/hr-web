0ee4bd6621fa6b518c0491981a8abba5beff28ed
samuelwondimu <wsamuelwondimu@gmail.com>
2023-06-20 11:03:58 +0300

    fix: Filter out sponsors faqs Link T-9299

--- src/app/(website)/(main)/advertise/page.tsx
+++ src/app/(website)/(main)/advertise/page.tsx
@@ -5,5 +5,6 @@ import AdvertisePage from "./components";
 
 export default async function Advertise() {
   const faqs = (await getAllFaqs()) as FaqType[];
-  return <AdvertisePage faqs={faqs} />;
+  const sponsersFaqs = faqs.filter((faq) => faq.faqType.includes("sponsor"));
+  return <AdvertisePage faqs={sponsersFaqs} />;
 }
