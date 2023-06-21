579184ad9f18cf5800654e3ecb1f618c2d81b883
samuelwondimu <wsamuelwondimu@gmail.com>
2023-06-21 11:11:39 +0300

    feat: Cornerstone Cta-1 comonent Link T-12012

--- src/components/Cornerstone/CornerstoneCta1.tsx
+++ src/components/Cornerstone/CornerstoneCta1.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
 import React from "react";
 
+import hoverStyles from "@/lib/hover";
+import { cx } from "@/lib/utils";
+
+import { GlowingButton, H4 } from "../ui";
+
 function CornerstoneCta1() {
-  return <div>CornerstoneCta1</div>;
+  return (
+    <div className="max-w-6xl px-2 py-16 mx-auto border rounded lg:py-26 border-gray-200/10 bg-slate-900 sm:py-24 lg:my-24 lg:flex lg:justify-between lg:px-16">
+      <div className="max-w-3xl">
+        <H4 as="h2" className="text-white ">
+          You've read this far, just subscribe already
+        </H4>
+        <p className="mt-3 text-lg leading-relaxed text-gray-300">
+          What's the worst that will happen? You'll get better at what you do,
+          enjoy a few laughs? It ain't hard...click the button below and give us
+          a shot.
+        </p>
+      </div>
+      <div className="mt-4 md:mt-5 md:max-w-xs">
+        <GlowingButton
+          variant="link"
+          href="/signup"
+          id="about-cta2"
+          aria-label="Go to sign-up form">
+          Level Up
+        </GlowingButton>
+        <p className="mt-4 text-xs leading-6 text-left text-gray-300">
+          We care about your{" "}
+          <Link
+            href="/privacy"
+            className={cx("font-bold text-white", hoverStyles)}>
+            privacy
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default CornerstoneCta1;
