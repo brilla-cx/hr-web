18c3737ecb268b720cddb8839c00cc761622b9ae
samuelwondimu <wsamuelwondimu@gmail.com>
2023-06-26 15:14:15 +0300

    fix: Intro section Link T-12280

--- src/components/Cornerstone/CornerstoneIntro.tsx
+++ src/components/Cornerstone/CornerstoneIntro.tsx
@@ -8,19 +8,14 @@ function CornerstoneIntro(props: IntroSection) {
     <div className="px-6 py-24 sm:py-32 lg:px-8">
       <div className="max-w-2xl mx-auto text-center">
         <h2 className="text-4xl font-bold tracking-tight text-gray-200 sm:text-6xl">
-          {props?.customHeading ||
-            `Static text with keyword ${props.keyword.keyword}`}
+          {props?.customizeHeading
+            ? props.customHeading
+            : `The juice on ${props.keyword.keyword} in 60 seconds`}
         </h2>
 
-        <article className="mt-6 text-lg leading-8 text-gray-400">
-          {props.content ? (
-            <PortableText value={props.content} />
-          ) : (
-            <p className="mt-6 text-lg leading-8 text-gray-400">
-              `Static content text with a keyword ${props.keyword.keyword}`
-            </p>
-          )}
-        </article>
+        <div className="mx-auto mt-10 text-gray-400">
+          <PortableText value={props.content} />
+        </div>
       </div>
     </div>
   );
