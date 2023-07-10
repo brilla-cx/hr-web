e31b6e1aea85c2ba6349c9f0cec77563d440c81e
samuelwondimu <wsamuelwondimu@gmail.com>
2023-07-10 11:22:02 +0300

    refactor: feed route code climate exceeding function lines Link T-11384

--- src/app/(website)/feed.xml/route.ts
+++ src/app/(website)/feed.xml/route.ts
@@ -6,34 +6,41 @@ import { FEED_OPTIONS, SITE_URL } from "@/lib/constants";
 import { getAllPosts } from "@/sanity/client";
 import { Post } from "@/types/types";
 
-export async function GET() {
+function generateFeedItem(post: Post, feed: RSS) {
+  const description = post.tldr ? blocksToHtmlString(post.tldr) : "";
+  const content = post.content ? blocksToHtmlString(post.content) : "";
+
+  feed.item({
+    title: post.name || "",
+    description: description || "",
+    url: `${SITE_URL}/gists/${post.slug.current}` || "",
+    date: post?.publishedAt || post._createdAt,
+    author: post.author?.name || "",
+    categories: post.categories?.map(({ name }) => name) || [],
+    custom_elements: [
+      {
+        "content:encoded": {
+          _cdata: content,
+        },
+      },
+    ],
+  });
+}
+
+async function generateFeed(): Promise<Response> {
   const posts = (await getAllPosts()) ?? [];
   const feedOptions: FeedOptions = FEED_OPTIONS;
   const feed = new RSS(feedOptions);
 
-  await posts?.map((post: Post) => {
-    const description = post.tldr ? `${blocksToHtmlString(post.tldr)}` : "";
-    const content = post.content ? `${blocksToHtmlString(post.content)}` : "";
-    return feed.item({
-      title: post.name ?? "",
-      description: description ?? "",
-      url: `${SITE_URL}/gists/${post.slug.current}` ?? "",
-      date: post?.publishedAt || post._createdAt,
-      author: post.author?.name ?? "",
-      categories: post.categories?.map(({ name }) => name) || [],
-      custom_elements: [
-        {
-          "content:encoded": {
-            _cdata: content,
-          },
-        },
-      ],
-    });
-  });
+  await Promise.all(posts.map((post: Post) => generateFeedItem(post, feed)));
 
   return new Response(feed.xml({ indent: true }), {
     headers: {
       "Content-Type": "application/atom+xml; charset=utf-8",
     },
   });
+}
+
+export function GET(): Promise<Response> {
+  return generateFeed();
 }
