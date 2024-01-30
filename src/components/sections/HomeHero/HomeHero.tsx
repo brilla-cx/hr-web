ad8d133325e71f98e71f9fbf59591f2107cc6cb2
Sam Rizvi <srizvi@users.noreply.github.com>
2024-01-29 23:57:12 -0800

    chore: update hero copy

--- src/components/sections/HomeHero/HomeHero.tsx
+++ src/components/sections/HomeHero/HomeHero.tsx
@@ -9,17 +9,13 @@ const HomeHero: React.FC = () => {
     <div className="px-2 py-16 sm:py-24 lg:px-16 lg:py-26">
       <div className="max-w-4xl">
         <HeroH1 className="text-gray-200">
-          AI enables the future
-          <br /> Choose to be part of it
+          Ditch the Grind
         </HeroH1>
         <Lead className="mt-5 max-w-3xl text-gray-400">
-          Hey Rebekah is a free daily newsletter. It's like Morning Brew for knowledge workers.
+          Unfiltered, practical insights from my entrepreneurial rollercoaster. Weekly fuel to accelerate business growth, boost profits, and restore peace of mind.
         </Lead>
         <Lead className="mt-5 max-w-3xl text-gray-400">
-          We're more concerned about human ambition than AI. Together, let's make sure that Sam—armed with GPT and their YouTube tutorials—doesn't have all the fun.
-        </Lead>
-        <Lead className="mt-5 max-w-3xl text-gray-400">
-          Join over 338,000 professionals in our community.
+          Join over 338,000 professionals in my community.
         </Lead>
         <div className="mt-8 md:mt-10 md:max-w-xs">
           <GlowingButton variant="link" href="/signup" id="hero" aria-label="Go to sign-up form">
